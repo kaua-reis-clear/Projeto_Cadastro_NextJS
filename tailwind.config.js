506a48ab6655583e786e2f988b12c65963f5a161
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-16 08:46:29 -0300

    created button component.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    /^bg-/,
+    /^to-/,
+    /^from-/,
+  ],
   theme: {
     extend: {},
   },
