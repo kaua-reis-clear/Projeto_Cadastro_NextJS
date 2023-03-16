ec544cfd466addd89b2f89dcb5b28d9bf0f8fbae
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-16 09:10:28 -0300

    created add client form component.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from "@/components/Botao"
+import Formulario from "@/components/Formulario"
 import Layout from "@/components/Layout"
 import Tabela from "@/components/Tabela"
 import Cliente from "@/core/Cliente"
@@ -30,6 +31,7 @@ export default function Home() {
         </div>
         <Tabela clientes={clientes}
         clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}/>
+        <Formulario cliente={clientes[2]}></Formulario>
       </Layout>
     </div>
   )
