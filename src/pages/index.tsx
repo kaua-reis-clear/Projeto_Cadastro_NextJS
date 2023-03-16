900d5890dff24f03998503b73f33c1c1aadcca73
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-16 09:32:03 -0300

    integrated table with form, passing selected data.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Cliente from "@/core/Cliente"
 import { useState } from "react"
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
   const clientes = [
     new Cliente('Ana', 34, '1'),
     new Cliente('Bia', 45, '2'),
@@ -14,18 +15,25 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome)
+    setCliente(cliente);
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente: Cliente) {
-    console.log(`Excluir... ${cliente.nome}`)
+    console.log(`Excluir... ${cliente.nome}`);
+  }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio());
+    setVisivel('form');
   }
 
   function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+    console.log(cliente);
+    setVisivel('tabela');
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela');
 
   return (
     <div className={`
@@ -37,7 +45,7 @@ export default function Home() {
           <>
           <div className="flex justify-end">
             <Botao cor="green" className="mb-4"
-            onClick={() => setVisivel('form')}>
+            onClick={novoCliente}>
               Novo Cliente
             </Botao>
             </div>
@@ -47,7 +55,7 @@ export default function Home() {
           </>
         ) : (
           <Formulario
-            cliente={clientes[2]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
           />
